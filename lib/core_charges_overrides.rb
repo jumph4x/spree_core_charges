913b0d289e8b69238a30f2f2770bcc24bc1f2aa9
Denis 'jumph4x <denis@fcpimport.com>
2011-06-01 15:08:29 -0400

    Deface style overrides

---
+++ lib/core_charges_overrides.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(:virtual_path => "admin/products/_form",
+                     :name => "converted_admin_product_form_right",
+                     :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                     :text => "<%= f.label :core_amount, t(\"core_amount\") %> <br />
+                               <%= f.select :core_amount, [nil, 25.0, 50.0, 75.0, 100.0, 150.0, 200.0], :class => \"text \"  %>
+                               <%= f.error_message_on :core_amount %>",
+                     :disabled => false)
