5ab104d0a4ca6690c7226b16ff1cdfbda8adbf45
Roman Smirnov <roman@railsdog.com>
2011-08-18 23:10:58 +0400

    Typo fix in Deface override

--- lib/core_charges_overrides.rb
+++ lib/core_charges_overrides.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "admin/products/_form",
                      :name => "converted_admin_product_form_right",
-                     :insert_after => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
+                     :insert_bottom => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
                      :text => "<%= f.label :core_amount, t(\"core_amount\") %> <br />
                                <%= f.select :core_amount, [nil, 25.0, 50.0, 75.0, 100.0, 150.0, 200.0], :class => \"text \"  %>
                                <%= f.error_message_on :core_amount %>",
