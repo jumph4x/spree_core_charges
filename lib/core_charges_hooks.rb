886063091cb8ae3d67dbbf1a3d4bd0d084eb5ee7
Roman Smirnov <roman@railsdog.com>
2011-08-18 23:09:11 +0400

    Revert "Typo in hook declaration"
    This reverts commit a41d3a1723ccfbdff1a485dea394affe25df7039.

--- lib/core_charges_hooks.rb
+++ lib/core_charges_hooks.rb
@@ -1,6 +1,6 @@
 class CoreChargesHooks < Spree::ThemeSupport::HookListener
 
-  insert_bottom :admin_product_form_right do
+  insert_after :admin_product_form_right do
     '<%= f.label :core_amount, t("core_amount") %> <br />
       <%= f.select :core_amount, [nil, 25.0, 50.0, 75.0, 100.0, 150.0, 200.0], :class => "text "  %>
       <%= f.error_message_on :core_amount %>'
