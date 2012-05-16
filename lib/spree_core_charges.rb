e26bf5c8ab5e765da2190948d02e765c3bace18a
Roman Smirnov <roman@railsdog.com>
2012-05-16 19:29:22 +0400

    Prevent recursion of AR callbacks

--- lib/spree_core_charges.rb
+++ lib/spree_core_charges.rb
@@ -13,21 +13,22 @@ module SpreeCoreCharges
       LineItem.class_eval do
         
         def update_adjustment(adjustment, source)
+          current_amount = adjustment.amount
           adjustment.amount = if adjustment.order(true).line_items.include? source
             calculate_core_charge
           else
             0
           end
-          
-          Adjustment.skip_callback :save, :after, :update_order
-          adjustment.save
-          Adjustment.set_callback :save, :after, :update_order
+          if current_amount != adjustment.amount
+            Adjustment.skip_callback :save, :after, :update_order
+            adjustment.save
+            Adjustment.set_callback :save, :after, :update_order
+          end
         end
         
         def calculate_core_charge
           return unless product.core_amount
-          
-          self.quantity * product.core_amount
+          - self.quantity * product.core_amount
         end
         
       private
