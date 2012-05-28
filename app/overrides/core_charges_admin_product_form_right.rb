9831d9c0345fe561344b19c5ad28727a419a1e24
Roman Smirnov <roman@railsdog.com>
2012-05-28 12:56:26 +0400

    Improved html code for core_amount fields in products form

--- app/overrides/core_charges_admin_product_form_right.rb
+++ app/overrides/core_charges_admin_product_form_right.rb
@@ -1,7 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "core_charges_admin_product_form_right",
                      :insert_bottom => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
-                     :text => "<%= f.label :core_amount, t(\"core_amount\") %> <br />
-                               <%= f.select :core_amount, [nil, 25.0, 50.0, 75.0, 100.0, 150.0, 200.0], :class => \"text \"  %>
-                               <%= f.error_message_on :core_amount %>",
+                     :partial => "admin/products/core_amount_fields",
                      :disabled => false)
