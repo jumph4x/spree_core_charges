afa71b26ccaded02c3c9433f7be802ee766cefe8
Denis Ivanov <visible.h4x@gmail.com>
2012-06-14 10:49:09 -0400

    Moving files into the spree namespace

--- app/overrides/core_charges_admin_product_form_right.rb
+++ app/overrides/core_charges_admin_product_form_right.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                      :name => "core_charges_admin_product_form_right",
                      :insert_bottom => "[data-hook='admin_product_form_right'], #admin_product_form_right[data-hook]",
-                     :partial => "admin/products/core_amount_fields",
+                     :partial => "spree/admin/products/core_amount_fields",
                      :disabled => false)
