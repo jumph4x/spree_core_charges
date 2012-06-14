af66e0366ae8f279e6a68290d14c318fb6579288
Denis Ivanov <visible.h4x@gmail.com>
2012-06-14 18:42:04 -0300

    This is a charge not a discount

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -19,6 +19,6 @@ Spree::LineItem.class_eval do
 
   def calculate_core_charge
     return unless product.core_amount
-    - self.quantity * product.core_amount
+    self.quantity * product.core_amount
   end
 end
