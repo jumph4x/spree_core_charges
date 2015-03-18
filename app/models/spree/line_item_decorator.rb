01f5160f53c877e7755060eaae27c80d81c51825
Roman Smirnov <POMAHC@gmail.com>
2015-03-18 13:11:39 +0300

    Updated to Spree 2.4 and remove some outdated code

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,22 +1,4 @@
 Spree::LineItem.class_eval do
-  def update_adjustment adjustment, source
-    new_amount = if adjustment.originator and source.order.line_items.include? adjustment.originator
-      calculate_core_charge
-    else
-      0
-    end
-
-    adjustment.update_column(:amount, new_amount)
-     
-   # Spree::Adjustment.skip_callback :save, :after, :update_adjustable
-   # adjustment.save
-   # Spree::Adjustment.set_callback :save, :after, :update_adjustable
-   end
-  
-  def eligible?(source = nil)
-    !!self.product.core_amount
-  end
-
   def calculate_core_charge
     return unless product.core_amount
     self.quantity * product.core_amount
--- spree_core_charges.gemspec
+++ spree_core_charges.gemspec
@@ -6,14 +6,14 @@ Gem::Specification.new do |s|
   s.version     = version
   s.authors     = ['Denis Ivanov']
   s.summary     = 'Ability to have basic core charges added to orders'
-  s.required_ruby_version = '>= 1.9.3'
+  s.required_ruby_version = '>= 2.1.0'
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  s.add_dependency('spree_core', '~> 2.1')
+  s.add_dependency('spree_core', '~> 2.4')
 
   s.add_development_dependency 'capybara'
   s.add_development_dependency 'factory_girl'
