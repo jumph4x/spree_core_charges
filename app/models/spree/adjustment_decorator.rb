0d599f66d18ab7c1dc470ec8d8686501bfeefcda
Roman Smirnov <POMAHC@gmail.com>
2015-05-21 12:22:04 +0300

    Added not_core scope to Adjustment

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,3 +1,4 @@
 Spree::Adjustment.class_eval do 
   scope :core, lambda { where('label like ?',"#{Spree.t(:core_charge)}%") }
+  scope :not_core, lambda { where('label not like ?',"#{Spree.t(:core_charge)}%") }
 end
