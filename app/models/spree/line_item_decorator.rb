e461024c7f8de0b1956edd13705a5c3c1ccf5390
Denis I <visible@jumph4x.net>
2015-06-28 13:28:19 -0700

    Spree 2.4 audit fixes

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,6 +1,26 @@
 Spree::LineItem.class_eval do
+  after_save :update_core_charges
+  before_destroy :destroy_core_charges
+
   def calculate_core_charge
     return unless product.core_amount
     self.quantity * product.core_amount
   end
+
+  def compute_amount o
+    calculate_core_charge
+  end
+
+  def core_charge
+    order.adjustments.find{|adj| adj.source_id == id}
+  end
+
+  def update_core_charges
+    core_charge and core_charge.update!
+  end
+
+  def destroy_core_charges
+    core_charge and core_charge.destroy
+  end
+
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -2,11 +2,11 @@ Spree::Order.class_eval do
 
   # Called each time an order is updated
   def create_core_charges
-    to_keep, to_destroy = adjustments.core.partition{|x| x.originator and x.originator.eligible?}
+    to_keep, to_destroy = adjustments.core.partition{|adj| adj.eligible?}
     to_destroy.each(&:delete)
-    core_variant_ids = to_keep.map{|x| x.originator.variant_id }
+    core_variant_ids = to_keep.map{|a| a.source.variant_id }
 
-    Spree::Adjustment.skip_callback :save, :after, :update_adjustable_adjustment_total
+    Spree::Adjustment.skip_callback :create, :after, :update_adjustable_adjustment_total
 
     line_items.each do |li|
       next unless li.product.core_amount # The product has no core charge associated with it
@@ -15,6 +15,7 @@ Spree::Order.class_eval do
       new_core_charge = Spree::Adjustment.new
       new_core_charge.label = "#{Spree.t(:core_charge)} [#{li.variant.sku}]"
       new_core_charge.source = li
+      new_core_charge.order = self
       new_core_charge.adjustable = self
       new_core_charge.amount = li.calculate_core_charge
       new_core_charge.eligible = true
@@ -22,14 +23,10 @@ Spree::Order.class_eval do
       new_core_charge.save
     end
 
-    Spree::Adjustment.set_callback :save, :after, :update_adjustable_adjustment_total
+    Spree::Adjustment.set_callback :create, :after, :update_adjustable_adjustment_total
 
     update_totals
-
-    update_columns({
-      :adjustment_total => adjustment_total,
-      :total => total
-    })
+    persist_totals
   end
 
 end
